05a755d4238394360cb6c2afef3de708c998fb72
natrdguerra <natalia.rdguerra@gmail.com>
2024-11-25 15:34:43 -0300

    Iniciando o projeto nodeJS

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import session from 'express-session';
 import cookieParser from 'cookie-parser';
-import path from 'path'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const app = express();
 
